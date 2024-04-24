3c2e0cfd5f9b7d4b055e1dd04ad71c74a2b7809b
Xivind <oivind.langeland@gmail.com>
2024-04-24 22:35:59 +0200

    Forbedret versjon, med ukefiltrering

--- lodd_generator.py
+++ lodd_generator.py
@@ -14,29 +14,27 @@ if __name__ == "__main__":
 
     RESULTS_FILE = config['CAMPAIGN'].get('RESULTS_FILE')
     TICKET_FILE = "lodd.txt"
-    TICKET_WEEKS = [14, 15]
+    TICKET_WEEKS = [16, 17]
 
     with open(RESULTS_FILE, 'r', encoding='utf-8') as file:
         datastore = json.load(file)
 
-    ticket_id = 1
     with open(TICKET_FILE, "w", encoding="utf-8") as file:
         unique_names = set()
-        two_tickets = 0
-        one_ticket = 0
-        no_tickets = 0
+        ticket_id = 0
         rows_written = 0
 
-        for athlete_id, athlete_data in datastore.items():
-            athlete_name = athlete_data["athlete_name"]
-            tickets = athlete_data["tickets"]
+        for athlete_data in datastore.values():
+            if athlete_data["week_number"] in TICKET_WEEKS:
+                athlete_name = athlete_data["athlete_name"]
+                tickets = athlete_data["tickets"]
 
-            for number in range(tickets):
-                file.write(f'{ticket_id} - {athlete_name}\n')
-                rows_written += 1
-                ticket_id = ticket_id+1
+                for number in range(tickets):
+                    rows_written += 1
+                    ticket_id += 1
+                    file.write(f'{ticket_id} - {athlete_name}\n')
 
-            unique_names.add(athlete_name)
+                unique_names.add(athlete_name)
 
     print(f"Kolleger med aktivitetstid: {len(unique_names)}")
     print(f"Totalt antall lodd utdelt: {rows_written}")
