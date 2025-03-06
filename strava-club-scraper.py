b40bb8decf55f9363894b74641fb3dc6742f9d56
thomiz <thomasrosenlund@gmail.com>
2025-03-06 18:13:52 +0100

    debug club_members_df

--- strava-club-scraper.py
+++ strava-club-scraper.py
@@ -1582,9 +1582,8 @@ def execution_time_to_google_sheets(*, sheet_id, sheet_name, timezone='UTC'):
 
 # Get data (via web-scraping)
 club_members_df = strava_club_members(
-#    strava_login=config['STRAVA']['LOGIN'],
-    strava_user=os.environ['STRAVA_USER'], #aktivitetsaksjonen
-    strava_password=os.environ['STRAVA_PASSWORD'], #aktivitetsaksjonen
+    strava_user=os.environ['STRAVA_USER'], 
+    strava_password=os.environ['STRAVA_PASSWORD'], 
     club_ids=config['STRAVA']['CLUB_IDS'].split(sep=', '),
     club_members_teams=club_members_teams,
     timezone=config['GENERAL']['TIMEZONE'],
